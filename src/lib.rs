4d84b8de2c72a695217fcd67462ff4e681b8b9d0
Alexandre Borghi <borghi.alexandre.12@gmail.com>
2023-04-06 11:27:13 +0200

    refactor: make main function return a Result
    Make the main function return a `Result<(), JsValue>` so we can use the
    `?` operator on fallible JS functions.

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,18 @@ use web_sys::window;
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 
 #[wasm_bindgen(start)]
-pub fn main() {
+pub fn main() -> Result<(), JsValue> {
     let window = window().unwrap();
     let document = window.document().unwrap();
     let canvas = document
-        .query_selector("canvas")
+        .query_selector("canvas")?
         .unwrap()
-        .unwrap()
-        .dyn_into::<HtmlCanvasElement>()
-        .unwrap();
+        .dyn_into::<HtmlCanvasElement>()?;
     let ctx = canvas
-        .get_context("2d")
+        .get_context("2d")?
         .unwrap()
-        .unwrap()
-        .dyn_into::<CanvasRenderingContext2d>()
-        .unwrap();
+        .dyn_into::<CanvasRenderingContext2d>()?;
     ctx.set_fill_style(&"white".into());
     ctx.fill_rect(10., 10., 200., 200.);
+    Ok(())
 }
